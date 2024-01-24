56c7a3a587f634c3bca9ca3838290c5bccc4b218
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2024-01-24 21:01:28 +0100

    Responsive navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,8 +1,10 @@
 'use client';
 
 import React from 'react';
+import { useState } from 'react';
 import styles from './links.module.css';
 import NavLink from './navLink/navLink';
+import Image from 'next/image';
 
 const links = [
   { title: 'Home', path: '/' },
@@ -12,22 +14,41 @@ const links = [
 ];
 
 const Links = () => {
+  const [open, setOpen] = useState(false);
+
   // FIXME:
   const session = true;
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{ title: 'Login', path: '/login' }} />
+    <div>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: 'Login', path: '/login' }} />
+        )}
+      </div>
+      <Image
+        className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   );
